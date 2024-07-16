025bc704294eb8019e8e979a63304d59fa1280ec
Narolskyi Denis <asotizianov@gmail.com>
2024-07-16 12:35:17 +0300

    fixed topbar, footer design

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+const toggleClass = (id, className) => {
+	const element = document.getElementById(id);
+	if (element) {
+		element.classList.toggle(className);
+	} else {
+		console.error('Element with ID ' + id + ' not found.');
+	}
+}
