bf2f25508be9ddb980b5367cdfd16461d58c983d
lizkyi <andriilizvinskyi@gmail.com>
2024-08-06 14:40:34 +0300

    add fixes

--- js/index.js
+++ js/index.js
@@ -6,3 +6,29 @@ const toggleClass = (id, className) => {
 		console.error('Element with ID ' + id + ' not found.');
 	}
 }
+
+
+/* ACCARDION CODE */
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionButtons = document.querySelectorAll(".accordion-button");
+
+    accordionButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            const content = this.nextElementSibling;
+            const activeContent = document.querySelector(".accordion-content[style]");
+
+            if (activeContent && activeContent !== content) {
+                activeContent.style.maxHeight = null;
+                activeContent.previousElementSibling.classList.remove("active");
+            }
+
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+                this.classList.remove("active");
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+                this.classList.add("active");
+            }
+        });
+    });
+});
